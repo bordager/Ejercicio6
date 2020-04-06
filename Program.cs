16bb51efe61b26a763bcbe7c0aa6676104a8fb8b
bordager <63132719+bordager@users.noreply.github.com>
2020-04-06 19:25:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca primer numero");
+            string numero1;
+            numero1 = Console.ReadLine();
+            Console.WriteLine("Introduzca segundo numero");
+            string numero2;
+            numero2 = Console.ReadLine();
+
+            long resultado = (int.Parse(numero1) + int.Parse(numero2)) * (int.Parse(numero1) - int.Parse(numero2));
+
+            Console.WriteLine("El resultado es " + resultado);
+
+            Console.Read(); 
+        }
+    }
+}
